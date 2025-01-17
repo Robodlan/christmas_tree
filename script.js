0be47156814bfe7d43e2e893886f94400334f8f4
Robodlan <azuldevs@zohomail.com>
2025-01-16 20:40:50 -0400

    tree working

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-const character = "🎸"
-const count = 15
+const character = "#"
+const count = 20
 const rows = []
 let inverted = false;
 
 function padRow(rowNumber, rowCount) {
-   return " ".repeat(rowCount - rowNumber) + character.repeat(1 * rowNumber - 1) + " ".repeat(rowCount - rowNumber)
+   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber)
 }
 
 
-
-for (let i = 1; i <= count; i++) {
-   if (inverted) {
-   rows.unshift(padRow(i, count));
- } else {
-   rows.push(padRow(i, count))
+while (rows.length < count) {
+  if (inverted) {
+    rows.unshift(padRow(rows.length + 1, count));
+  } else {
+    rows.push(padRow(rows.length + 1, count));
   }
 }
-// while (rows.length < count) {
-//   rows.push(padRow(rows.length + 1, count));
-// }
-
-// for (let i = count; i > 0; i--) {
-//   rows.push(padRow(i, count));
-// }
-
-
-
-// for (let i = 1; i <= count; i++) { 
-//    rows.push(padRow(i , count))
-// }
 
 let result = ""
 
@@ -36,5 +22,5 @@ for (const row of rows) {
     result +=  row + "\n"
 }
 
-console.log(result);
+document.getElementById("tree").textContent = result;
 
